d2325bdd9e0f3b46685eddeeda3c7c0df18aa685
sh32321 <76272506+sh32321@users.noreply.github.com>
2021-08-04 13:05:04 -0700

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+    GamePanel panel;
+
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("This is a Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.lightGray);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
